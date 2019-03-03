797887219f0f7571a7ef789f86fc831425caa139
wajahat <wajahat.ahmed245@gmail.com>
2019-03-03 18:12:08 +0500

    jinja templating

--- mysite/flights/views.py
+++ mysite/flights/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Flight
+from django.http import  Http404
 
 
 # Create your views here.
@@ -9,3 +10,17 @@ def index(request):
         "flights": Flight.objects.all()
     }
     return render(request, "flights/index.html", context)
+
+
+def flights(request, flight_id):
+    try:
+        flights_id = Flight.objects.get(pk=flight_id)
+
+    except Flight.DoesNotExist:
+        raise Http404("Flight do Title's not exist")
+
+    context = {
+        "flights": flights_id
+    }
+
+    return render(request, "flights/flights.html", context)
