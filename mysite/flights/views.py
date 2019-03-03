22d12ee46ce9e65ebaf08cc44d7ca45b58a5fbd7
wajahat <wajahat.ahmed245@gmail.com>
2019-03-03 16:35:06 +0500

    test commit using pycharm 2

--- mysite/flights/views.py
+++ mysite/flights/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Flight
+
 
 # Create your views here.
+
+def index(request):
+    context = {
+        "flights": Flight.objects.all()
+    }
+    return render(request, "flights/index.html", context)
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path("", include('hello.urls')),
+    path("", include('flights.urls')),
     path('admin/', admin.site.urls),
 ]
